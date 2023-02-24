29476bbb1819de6703be4af7d372b86fb62817e9
Bohdan <bodya.ru02@gmail.com>
2023-02-24 13:41:27 +0200

    Refactor and remove unneeded libraries from CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,14 @@
 #define GLFW_EXPOSE_NATIVE_COCOA
 #endif
 #include <GLFW/glfw3native.h>
-#include "logo.hpp"
 
 static bool s_showStats = false;
 
-static void glfw_errorCallback(int error, const char *description)
-{
+static void glfw_errorCallback(int error, const char *description) {
     fprintf(stderr, "GLFW error %d: %s\n", error, description);
 }
 
-static void glfw_keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
+static void glfw_keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
         s_showStats = !s_showStats;
 }
@@ -31,8 +28,9 @@ int main(int argc, char **argv) {
     glfwSetErrorCallback(glfw_errorCallback);
     if (!glfwInit())
         return 1;
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(1024, 768, "helloworld", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "minesraft", nullptr, nullptr);
     if (!window)
         return 1;
     glfwSetKeyCallback(window, glfw_keyCallback);
@@ -41,6 +39,7 @@ int main(int argc, char **argv) {
     bgfx::renderFrame();
     // Initialize bgfx using the native window handle and window resolution.
     bgfx::Init init;
+
 #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
     init.platformData.ndt = glfwGetX11Display();
     init.platformData.nwh = (void *) (uintptr_t) glfwGetX11Window(window);
@@ -49,17 +48,21 @@ int main(int argc, char **argv) {
 #elif BX_PLATFORM_WINDOWS
     init.platformData.nwh = glfwGetWin32Window(window);
 #endif
+
     int width, height;
     glfwGetWindowSize(window, &width, &height);
     init.resolution.width = (uint32_t) width;
     init.resolution.height = (uint32_t) height;
     init.resolution.reset = BGFX_RESET_VSYNC;
+
     if (!bgfx::init(init))
         return 1;
+
     // Set view 0 to the same dimensions as the window and to clear the color buffer.
     const bgfx::ViewId kClearView = 0;
-    bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
+    bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x44ff);
     bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         // Handle window resize.
@@ -73,24 +76,19 @@ int main(int argc, char **argv) {
         bgfx::touch(kClearView);
         // Use debug font to print information about this example.
         bgfx::dbgTextClear();
-        bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(width / 2 / 8), 20) - 20,
-                           bx::max<uint16_t>(uint16_t(height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
         bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-        bgfx::dbgTextPrintf(0, 1, 0x0f,
-                            "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-        bgfx::dbgTextPrintf(80, 1, 0x0f,
-                            "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-        bgfx::dbgTextPrintf(80, 2, 0x0f,
-                            "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
         const bgfx::Stats *stats = bgfx::getStats();
-        bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.",
+        bgfx::dbgTextPrintf(0, 1, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.",
                             stats->width, stats->height, stats->textWidth, stats->textHeight);
         // Enable stats or debug text.
         bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
+
         // Advance to next frame. Process submitted rendering primitives.
         bgfx::frame();
     }
+
     bgfx::shutdown();
     glfwTerminate();
+
     return 0;
 }
